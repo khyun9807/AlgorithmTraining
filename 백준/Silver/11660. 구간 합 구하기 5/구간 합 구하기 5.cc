ed62708bd0dbd9a8375b84b08097ce0759e1306b
kyung hyun <131232309+khyun9807@users.noreply.github.com>
2025-04-18 16:00:18 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 164 ms, Memory: 9136 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.cc
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.cc
@@ -1,19 +1,40 @@
 #include <iostream>
+#include <string>
 #include <deque>
 
 using namespace std;
 
-int arr[1025][1025] = { 0, };
 int arraccul[1025][1025] = { 0, };
 
 int n, m;
 
+void parsing(string& cmd, int* arr) {
+	int i = 0;
+	int num = 0;
+	for (char& c : cmd) {
+		if (c == ' ') {
+			arr[i] = num;
+			num = 0;
+			++i;
+		}
+		else {
+			num = num * 10 + (c - '0');
+		}
+	}
+	arr[i] = num;
+}
+
 void init() {
 	cin >> n >> m;
+	cin.ignore();
 	for (int i = 1; i <= n; i++) {
-		for (int j = 1; j <= n; j++) {
-			cin >> arr[i][j];
-			arraccul[i][j] = arraccul[i][j - 1] + arr[i][j];
+		int temp[1025];
+		string cmd;
+		getline(cin, cmd);
+		parsing(cmd, temp);
+
+		for (int j = 1; j <= n; j++) {			
+			arraccul[i][j] = arraccul[i][j - 1] + temp[j-1];
 		}
 	}
 }
@@ -29,17 +50,32 @@ int main() {
 
 	init();
 
+	string r = "";
+
 	while (m-- > 0) {
+		//int x1, y1, x2, y2;
+		//cin >> x1 >> y1 >> x2 >> y2;
+
+		int arr[4];
+
+		string cmd;
+		getline(cin, cmd);
+		parsing(cmd, arr);
 		int x1, y1, x2, y2;
-		cin >> x1 >> y1 >> x2 >> y2;
+		x1 = arr[0];
+		y1 = arr[1];
+		x2 = arr[2];
+		y2 = arr[3];
 
 		long long sum = 0;
 		for (int i = x1; i <= x2; i++) {
 			sum += (getAcculSum(i, y2) - getAcculSum(i, y1 - 1));
 		}
-		cout << sum << "\n";
+
+		r += (to_string(sum) + "\n");
 	}
 
+	cout << r;
 
 	return 0;
 }
