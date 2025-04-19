8950fbffb30b92fca6906dea1f6018eb5a239c23
kyung hyun <131232309+khyun9807@users.noreply.github.com>
2025-04-19 10:32:16 +0900

    [Gold III] Title: 나머지 합, Time: 60 ms, Memory: 34528 KB -BaekjoonHub

--- 백준/Gold/10986. 나머지 합/나머지 합.cc
+++ 백준/Gold/10986. 나머지 합/나머지 합.cc
@@ -1,58 +1,74 @@
-#include<iostream>
-#include<string>
-#include<deque>
-#include<algorithm>
+#include <iostream>
+#include <string>
 
 using namespace std;
 
-//100,000,000
-//256 1024 1024 256,000,000
+// 1초 100,000,000연산
+// 256 * 1024 * 1024 바이트
+
 //N 1,000,000
-//M 1,000
-//A 1,000,000,000
+//ai < 10억 => int
+
+//19 % 3 = 1
+//(3 + 8 + 8) % 3
+// (4 + 7 + 8) %3
+
+void parse(string& cmd, long long* arr) {
+	int i = 1;
+	long long num = 0;
+	for (char& c : cmd) {
+		if (c == ' ') {
+			arr[i] = num;
+			i++;
+			num = 0;
+		}
+		else {
+			num = num * 10 + (c - '0');
+		}
+	}
+
+	arr[i] = num;
+}
+
+long long countarr[1001] = { 0, };
+long long arr[1000001] = { 0, };
 
 int main() {
 	ios::sync_with_stdio(false);
 	cin.tie(0);
 	cout.tie(0);
 
+
 	int n, m;
 	cin >> n >> m;
+	cin.ignore();
 
-	deque<long long> dq;
-	dq.push_back(0);
+	string cmd;
+	getline(cin, cmd);
+	parse(cmd, arr);
 
-	for (int i = 0; i <= n - 1; i++) {
-		int x;
-		cin >> x;
-		dq.push_back(dq.at(i)+x);
-	}
-	dq.pop_front();
-	//1 2 3 1 2
-	//1 3 6 7 9
-	//1 0 0 1 0
 
-	for (int i = 0; i < dq.size(); i++) {
-		dq.at(i) %= m;
+	for (int i = 1; i <= n; i++) {
+
+		arr[i] += arr[i - 1];
 	}
 
-	long long count[1001] = { 0, };
 
-	for (int i = 0; i < dq.size(); i++) {
-		count[dq.at(i)]++;
+	for (int i = 1; i <= n; i++) {
+		countarr[arr[i] % m]++;
 	}
 
-	long long sum = count[0];
-
-	for (int i = 0; i <= m-1; i++) {
-		long long c = count[i];
+	long long sum = countarr[0];
+	for (int i = 0; i <= m - 1; i++) {
+		long long c = countarr[i];
 		if (c >= 2) {
-			sum += (c*(c-1)/2);
+			sum += (c * (c - 1) / 2);
 		}
 	}
-
 
 	cout << sum;
 
 	return 0;
 }
+
+//1 3 6 7 9
