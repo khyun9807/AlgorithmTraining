f2137c4e7e7851955e1a3e1a54b5705da266c990
kyung hyun <131232309+khyun9807@users.noreply.github.com>
2026-03-30 13:35:40 +0900

    [Gold V] Title: 탑, Time: 120 ms, Memory: 6140 KB -BaekjoonHub

--- 백준/Gold/2493. 탑/탑.cc
+++ 백준/Gold/2493. 탑/탑.cc
@@ -3,41 +3,44 @@
 
 using namespace std;
 
+deque<int> height;
+deque<int> indexRecord;
+int n;
+
 int main() {
-	ios::sync_with_stdio(false);
 	cin.tie(0);
+	cout.tie(0);
+	ios::sync_with_stdio(false);
 
-	deque<pair<int,int>> dq;//<height,jmp idx>
-	dq.push_back(pair<int,int>(100000001,0));
+	cin >> n;
+	height.assign(n+1, 0);
+	indexRecord.assign(n+1, 0);
+	height.at(0) = 100,000,001;
 
-	int N;
-	cin >> N;
-
-	int i = 1;
-	while (i <= N) {
+	for (int i = 1; i <= n; i++) {
 		int x;
 		cin >> x;
 
-		int idx = dq.size() - 1;
-		while (1) {
-			if (x <= dq[idx].first) {
-				break;
-			}
-			else {
-				idx = dq[idx].second;
+		height.at(i) = x;
+
+		if (i != 1) {
+			int searchIdx = i-1;
+
+			while (searchIdx != 0) {
+				if (height.at(searchIdx) >= height.at(i)) {
+					indexRecord.at(i) = searchIdx;
+					break;
+				}
+				else {
+					searchIdx = indexRecord.at(searchIdx);
+				}
 			}
 		}
-
-
-		dq.push_back(pair<int, int>(x,idx));
-
-		++i;
 	}
 
-	int idx = dq.size() - 1;
-
-	for (int i = 1; i <= idx; i++) {
-		cout << dq[i].second << " ";
+	indexRecord.pop_front();
+	for (int& x : indexRecord) {
+		cout << x << " ";
 	}
 
 	return 0;
