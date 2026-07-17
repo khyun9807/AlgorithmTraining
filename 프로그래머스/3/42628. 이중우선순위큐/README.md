ca9ad35690802b2524966e003494cf5518ff04e6
kyung hyun <131232309+khyun9807@users.noreply.github.com>
2026-07-17 11:09:47 +0900

    [level 3] Title: 이중우선순위큐, Time: 43.02 ms, Memory: 126 MB -BaekjoonHub

--- 프로그래머스/3/42628. 이중우선순위큐/README.md
+++ 프로그래머스/3/42628. 이중우선순위큐/README.md
@@ -1,10 +1,10 @@
 # [level 3] 이중우선순위큐 - 42628 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42628?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42628) 
 
 ### 성능 요약
 
-메모리: 140 MB, 시간: 62.40 ms
+메모리: 126 MB, 시간: 43.02 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 07월 15일 15:51:23
+2026년 07월 17일 11:09:43
 
 ### 문제 설명
 
--- 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
@@ -1,43 +1,81 @@
 import java.util.*;
 
 class Solution {
-    PriorityQueue<Integer> maxpq=new PriorityQueue<>(Collections.reverseOrder());
-    PriorityQueue<Integer> minpq=new PriorityQueue<>();
-    
     public int[] solution(String[] operations) {
-        int[] answer = {0,0};
+        int[] answer = new int[2];
+        PriorityQueue<Integer> pqmax=new PriorityQueue<>(Collections.reverseOrder());
+        PriorityQueue<Integer> pqmin=new PriorityQueue<>();
+        
+        Map<Integer,Integer> livemap=new HashMap<>();
         
         for(String op:operations){
-            String[] s=op.split(" ");
-            int x=Integer.parseInt(s[1]);
+            char c=op.charAt(0);
+            int num=Integer.parseInt(op.substring(2));
             
-            if(s[0].equals("I")){
-                maxpq.add(x);
-                minpq.add(x);
+            if(c=='I'){
+                pqmax.add(num);
+                pqmin.add(num);
+                int val=livemap.getOrDefault(num,0);
+                livemap.put(num,val+1);
             }
             else{
-                if(x==1){
-                    Integer y=maxpq.poll();
-                    if(y!=null){
-                        minpq.remove(y);
+                if(num==1){//remove max
+                    while(!pqmax.isEmpty()){
+                        int cur=pqmax.poll();
+                        int val=livemap.get(cur);
+                        if(val<=0){
+                            continue;
+                        }
+                        else{
+                            livemap.replace(cur,val-1);
+                            break;
+                        }
                     }
                 }
-                else{
-                    Integer y=minpq.poll();
-                    if(y!=null){
-                        maxpq.remove(y);
+                else{//rmv min
+                    while(!pqmin.isEmpty()){
+                        int cur=pqmin.poll();
+                        int val=livemap.get(cur);
+                        if(val<=0){
+                            continue;
+                        }
+                        else{
+                            livemap.replace(cur,val-1);
+                            break;
+                        }
                     }
                 }
             }
         }
         
-        if(!maxpq.isEmpty()){
-            answer[0]=maxpq.peek();
+        int mx=0;
+        int mn=0;
+        
+        while(!pqmax.isEmpty()){
+            int cur=pqmax.poll();
+            int val=livemap.get(cur);
+            if(val<=0){
+                continue;
+            }
+            else{
+                mx=cur;
+                break;
+            }
+        }
+        while(!pqmin.isEmpty()){
+            int cur=pqmin.poll();
+            int val=livemap.get(cur);
+            if(val<=0){
+                continue;
+            }
+            else{
+                mn=cur;
+                break;
+            }
         }
         
-        if(!minpq.isEmpty()){
-            answer[1]=minpq.peek();
-        }
+        answer[0]=mx;
+        answer[1]=mn;
         
         return answer;
     }
